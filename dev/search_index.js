92961e52f90a42fc8a6e325d9104d17f48feaae6
Documenter.jl <documenter@juliadocs.github.io>
2025-09-08 19:16:59 +0000

    build based on d5443c6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#PhyloSummaries","page":"Home","title":"PhyloSummaries","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PhyloSummaries.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
